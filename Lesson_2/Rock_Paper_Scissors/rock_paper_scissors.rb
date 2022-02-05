d288e3e164cf7a98320dd774a2a2d5f1fe4b2810
tsangsiu <tsangsiu@gmail.com>
2022-02-05 23:21:39 +0800

    refactored after rubocop

--- Lesson_2/Rock_Paper_Scissors/rock_paper_scissors.rb
+++ Lesson_2/Rock_Paper_Scissors/rock_paper_scissors.rb
@@ -15,14 +15,16 @@ def prompt(message)
   puts add_prompt(message)
 end
 
+def first_player_win?(first_player, second_player)
+  (first_player == 'Rock' && second_player == 'Scissors') ||
+    (first_player == 'Paper' && second_player == 'Rock') ||
+    (first_player == 'Scissors' && second_player == 'Paper')
+end
+
 def display_result(player_choice, computer_choice)
-  if (player_choice == 'Rock' && computer_choice == 'Scissors') ||
-     (player_choice == 'Paper' && computer_choice == 'Rock') ||
-     (player_choice == 'Scissors' && computer_choice == 'Paper')
+  if first_player_win?(player_choice, computer_choice)
     prompt("You won!")
-  elsif (player_choice == 'Rock' && computer_choice == 'Paper') ||
-        (player_choice == 'Paper' && computer_choice == 'Scissors') ||
-        (player_choice == 'Scissors' && computer_choice == 'Rock')
+  elsif first_player_win?(computer_choice, player_choice)
     prompt("Computer won!")
   else
     prompt("It's a tie!")
@@ -31,8 +33,8 @@ end
 
 # Constants
 
-CHOICES = ['Rock', 'Paper', 'Scissors']
-YES_NO = ['Yes', 'No']
+CHOICES = %w(Rock Paper Scissors)
+YES_NO = %w(Yes No)
 
 # Main Program
 
@@ -42,7 +44,7 @@ loop do
   player_choice = prompt.select(add_prompt('Choose one:'), CHOICES, cycle: true)
   computer_choice = CHOICES.sample
 
-  prompt("You chose #{player_choice.downcase}, " + 
+  prompt("You chose #{player_choice.downcase}, " \
          "and computer chose #{computer_choice.downcase}.")
   display_result(player_choice, computer_choice)
 
