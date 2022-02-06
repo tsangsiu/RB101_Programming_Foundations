54e38ccae98d3ea8d435da63c257f6d50959a19a
tsangsiu <tsangsiu@gmail.com>
2022-02-07 07:40:33 +0800

    done #6 of Medium 1

---
+++ Small_Problems/11_Medium_1/06.rb
@@ -0,0 +1,28 @@
+def minilang(program)
+  stack = []
+  register = 0
+
+  program.split.each do |command|
+    case command
+    when 'PUSH'  then stack << register
+    when 'ADD'   then register += stack.pop
+    when 'SUB'   then register -= stack.pop
+    when 'MULT'  then register *= stack.pop
+    when 'DIV'   then register /= stack.pop
+    when 'MOD'   then register %= stack.pop
+    when 'POP'   then register = stack.pop
+    when 'PRINT' then puts register
+    else              register = command.to_i
+    end
+  end
+end
+
+# minilang('PRINT')
+# minilang('5 PUSH 3 MULT PRINT')
+# minilang('5 PRINT PUSH 3 PRINT ADD PRINT')
+# minilang('5 PUSH POP PRINT')
+# minilang('3 PUSH 4 PUSH 5 PUSH PRINT ADD PRINT POP PRINT ADD PRINT')
+# minilang('3 PUSH PUSH 7 DIV MULT PRINT ')
+# minilang('4 PUSH PUSH 7 MOD MULT PRINT ')
+# minilang('-3 PUSH 5 SUB PRINT')
+# minilang('6 PUSH')
