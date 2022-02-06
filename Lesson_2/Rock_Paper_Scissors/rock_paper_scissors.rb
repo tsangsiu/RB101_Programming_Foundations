426b6e52b67b62d5117612cf0c071ab8cdc92735
tsangsiu <tsangsiu@gmail.com>
2022-02-06 17:14:32 +0800

    amend the game so that 3 wins win the game

--- Lesson_2/Rock_Paper_Scissors/rock_paper_scissors.rb
+++ Lesson_2/Rock_Paper_Scissors/rock_paper_scissors.rb
@@ -11,23 +11,46 @@ def prompt(message)
   puts add_prompt(message)
 end
 
-def first_player_win?(first_player, second_player)
-  CHOICES_TO_WIN[first_player].include?(second_player)
+def win?(player_choice, computer_choice)
+  CHOICES_TO_WIN[player_choice].include?(computer_choice)
 end
 
-def display_result(player_choice, computer_choice)
-  if first_player_win?(player_choice, computer_choice)
-    prompt("You won!")
-  elsif first_player_win?(computer_choice, player_choice)
-    prompt("Computer won!")
+def add_score(scores, player_choice, computer_choice)
+  if win?(player_choice, computer_choice)
+    scores[:player] += 1
+  elsif win?(computer_choice, player_choice)
+    scores[:computer] += 1
+  end
+end
+
+def display_round_result(player_choice, computer_choice)
+  if win?(player_choice, computer_choice)
+    prompt("You win this round!")
+  elsif win?(computer_choice, player_choice)
+    prompt("Computer wins this round!")
   else
     prompt("It's a tie!")
+  end
+end
+
+def display_grand_result(scores)
+  if scores[:player] == WINS_NEEDED
+    message = "You win the game! Congulations!"
+    prompt("*" * message.length)
+    prompt(message)
+    prompt("*" * message.length)
+  elsif scores[:computer] == WINS_NEEDED
+    message = "Computer wins the game!"
+    prompt("*" * message.length)
+    prompt(message)
+    prompt("*" * message.length)
   end
 end
 
 # Constants
 
 YES_NO = %w(Yes No)
+WINS_NEEDED = 3
 CHOICES = %w(Rock Paper Scissors Spock Lizard)
 CHOICES_TO_WIN = {
   'Scissors' => %w(Paper Lizard),
@@ -39,15 +62,32 @@ CHOICES_TO_WIN = {
 
 # Main Program
 
-system 'clear'
-
 loop do
-  player_choice = prompt.select(add_prompt('Choose one:'), CHOICES, cycle: true)
-  computer_choice = CHOICES.sample
+  system 'clear'
 
-  prompt("You chose #{player_choice.downcase}, " \
-         "and computer chose #{computer_choice.downcase}.")
-  display_result(player_choice, computer_choice)
+  scores = { player: 0, computer: 0 }
+
+  loop do
+    player_choice = prompt.select(add_prompt('Choose one:'),
+                                  CHOICES,
+                                  cycle: true)
+    computer_choice = CHOICES.sample
+
+    system 'clear'
+
+    prompt("You chose #{player_choice.downcase}, " \
+           "and computer chose #{computer_choice.downcase}.")
+    display_round_result(player_choice, computer_choice)
+
+    add_score(scores, player_choice, computer_choice)
+    prompt("Your Score: #{scores[:player]}; " \
+           "Computer Score: #{scores[:computer]}")
+
+    if scores.values.include?(WINS_NEEDED)
+      display_grand_result(scores)
+      break
+    end
+  end
 
   play_again = prompt.select(add_prompt('Do you want to play again?'),
                              YES_NO,
