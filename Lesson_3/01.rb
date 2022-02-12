37c63de8a4d4c7edce7b4f9d1e8a28a0c7806cc6
tsangsiu <tsangsiu@gmail.com>
2022-02-12 14:22:40 +0800

    done Hard 1

---
+++ Lesson_3/01.rb
@@ -0,0 +1,14 @@
+if false
+  greeting = "hello world"
+end
+
+p greeting # => nil
+
+###
+
+loop do
+  variable
+  break
+end
+
+p variable # exception is thrown
---
+++ Lesson_3/04.rb
@@ -0,0 +1,17 @@
+def is_an_ip_number?(number)
+  (0..255).cover?(number.to_i)
+end
+
+def dot_separated_ip_address?(input_string)
+  dot_separated_words = input_string.split(".")
+  return false if dot_separated_words.size != 4
+
+  while dot_separated_words.size > 0 do
+    word = dot_separated_words.pop
+    return false unless is_an_ip_number?(word)
+  end
+
+  true
+end
+
+p dot_separated_ip_address?('10.255.11.3')
