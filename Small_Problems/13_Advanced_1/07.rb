bf182da0fb2c1e3717d31e4480792dccf8d38364
tsangsiu <tsangsiu@gmail.com>
2022-05-16 21:10:22 +0800

    Implement a new algorithm to merge sorted lists

--- Small_Problems/13_Advanced_1/07.rb
+++ Small_Problems/13_Advanced_1/07.rb
@@ -43,7 +43,38 @@ def merge(arr1, arr2)
   merged_arr
 end
 
-p merge([1, 5, 9], [2, 6, 8]) == [1, 2, 5, 6, 8, 9]
-p merge([1, 1, 3], [2, 2]) == [1, 1, 2, 2, 3]
-p merge([], [1, 4, 5]) == [1, 4, 5]
-p merge([1, 4, 5], []) == [1, 4, 5]
+=begin
+
+# Another Algorithm
+[1, 5, 9] [2, 6, 8]
+- Initialize `index2` to `0`, that indicates the element index in the second Array
+- Initialize `merged_arr` to an empty Array
+- Iterate through the first Array
+  - While `index2` is less than the size of `arr2` and `arr2[index2]` is less than the current element,
+    - Push `arr2[index2]` to `merged_arr`
+    - Increment `index2` by 1
+  - Push the current element from the first Array to `merged_arr`
+- Push the remaining elements from the second Array to `merged_arr`
+- Return `merged_arr`
+
+=end
+
+def merge(arr1, arr2)
+  merged_arr = []
+  index2 = 0
+
+  arr1.each do |element|
+    while index2 < arr2.size && arr2[index2] < element
+      merged_arr << arr2[index2]
+      index2 += 1
+    end
+    merged_arr << element
+  end
+  
+  merged_arr += arr2[index2..-1]
+end
+
+p merge([1, 5, 9], [2, 6, 8]) #== [1, 2, 5, 6, 8, 9]
+p merge([1, 1, 3], [2, 2]) #== [1, 1, 2, 2, 3]
+p merge([], [1, 4, 5]) #== [1, 4, 5]
+p merge([1, 4, 5], []) #== [1, 4, 5]
